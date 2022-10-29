17841f969ae7bad5a197caae2b1f469cbce447ac
Bruno Bereoff <bbereoff@gmail.com>
2022-10-29 15:07:56 -0300

    removed: message contents from home view

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,6 @@
 
 import os
 
-from django.contrib import messages
 from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
@@ -16,8 +15,6 @@ def home(request):
     recipes = Recipe.objects.filter(
         is_published=True,
     ).order_by('-id')
-
-    messages.success(request, 'Test message.')
 
     page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
