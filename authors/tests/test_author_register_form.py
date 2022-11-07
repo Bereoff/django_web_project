025223fd05a950234bd7e5fe7fecc37b3772de9a
Bruno Bereoff <bbereoff@gmail.com>
2022-11-07 20:50:26 -0300

    added: form test routine

---
+++ authors/tests/test_author_register_form.py
@@ -0,0 +1,50 @@
+
+from django.test import TestCase
+from parameterized import parameterized
+
+from authors.forms import RegisterForm
+
+
+class AuthorRegisterFormUnitTest(TestCase):
+    @parameterized.expand([
+        ('username', 'Your username'),
+        ('email', 'Your e-mail'),
+        ('first_name', 'Ex.: John'),
+        ('last_name', 'Ex.: Doe'),
+        ('password', 'Type your password'),
+        ('password2', 'Repeat your password'),
+    ])
+    def test_fields_placeholder(self, field, placeholder):
+        form = RegisterForm()
+        current_placeholder = form[field].field.widget.attrs['placeholder']
+        self.assertEqual(current_placeholder, placeholder)
+
+    @parameterized.expand([
+        ('username', (
+            'Required. 150 characters or fewer. '
+            'Letters, digits and @/./+/-/_ only.'
+        )),
+        ('email', 'The e-mail must be valid.'),
+        ('password', (
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        )),
+    ])
+    def test_fields_help_text(self, field, needed):
+        form = RegisterForm()
+        current = form[field].field.help_text
+        self.assertEqual(current, needed)
+
+    @parameterized.expand([
+        ('first_name', 'First Name'),
+        ('last_name', 'Last Name'),
+        ('username', 'Username'),
+        ('email', 'Email'),
+        ('password', 'Password'),
+        ('password2', 'Password2'),
+    ])
+    def test_fields_label(self, field, needed):
+        form = RegisterForm()
+        current = form[field].field.label
+        self.assertEqual(current, needed)
